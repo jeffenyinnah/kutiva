5d98ae4829a4cc601b3a751c5093204d3578c368
jose machava <jose.s.machava@gmail.com>
2018-04-22 14:22:47 +0200

    add username in required fields

--- account/models.py
+++ account/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     email = models.EmailField(('email address'), unique=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
 #
 # class Perfil(models.Model):
