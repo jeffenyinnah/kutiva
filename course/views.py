dd3e0d1cb94f2e17716ffa9c0376de0cebad89d9
jose machava <jose.s.machava@gmail.com>
2018-04-23 09:47:47 +0200

    add course style

--- course/views.py
+++ course/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import  *
 # Create your views here.
 
 
 def course(request):
-    return render(request ,'course/index.html')
+    courses = Course.objects.all()
+    categories = Category.objects.all()
+    return render(request,'course/index.html',{"courses": courses, 'categories':categories})
